d718e856a705c80cc060b8bc2e2e74b34a64ac81
thukarala <arvind.pratap2020@vitbhopal.ac.in>
2023-06-11 22:40:07 +0530

    Don

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonfile = require('jsonfile');
 const moment = require('moment');
 const FILE_PATH = "data.json";
 const simplegit = require('simple-git');
-const DATE = moment().subtract(110, 'd').format();
+const DATE = moment().subtract(105, 'd').format();
 
 const data = {
     date : DATE
