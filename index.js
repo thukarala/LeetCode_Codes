81ec842e11717ebf0a6c8e20f1c521d8b80c1091
thukarala <arvind.pratap2020@vitbhopal.ac.in>
2023-06-11 17:11:57 +0530

    Update

---
+++ index.js
@@ -0,0 +1,13 @@
+const jsonfile = require('jsonfile');
+const moment = require('moment');
+const FILE_PATH = "data.json";
+const simplegit = require('simple-git');
+const DATE = moment().subtract(21 , 'd').format();
+
+const data = {
+    date : DATE
+}
+jsonfile.writeFile(FILE_PATH , data);
+
+simplegit().add([FILE_PATH]).commit(DATE , {'--date':DATE});
+
