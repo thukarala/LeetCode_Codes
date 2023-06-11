94c85d29856b544d33e73ddcbd43e80703c1ba67
thukarala <arvind.pratap2020@vitbhopal.ac.in>
2023-06-11 17:36:57 +0530

    Done

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const jsonfile = require('jsonfile');
 const moment = require('moment');
 const FILE_PATH = "data.json";
 const simplegit = require('simple-git');
-const DATE = moment().subtract(11 , 'd').format();
+const DATE = moment().subtract(1 , 'd').format();
 
 const data = {
     date : DATE
 }
-jsonfile.writeFile(FILE_PATH , data);
+jsonfile.writeFile(FILE_PATH , data , () => {
+    simplegit().add([FILE_PATH]).commit(DATE , {'--date':DATE});
+});
 
-simplegit().add([FILE_PATH]).commit(DATE , {'--date':DATE});
 
